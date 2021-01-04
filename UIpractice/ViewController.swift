2947c7b412ca01880bcada2e79edace8be98fad7
yousef <ybajafar@gmail.com>
2021-01-05 00:28:20 +0300

    Initial Commit

---
+++ UIpractice/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  UIpractice
+//
+//  Created by yousef bj on 21/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+   
+        
+        
+    }
+
+
+}
+
